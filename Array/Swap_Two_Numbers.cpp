483fd6d37525460bc4b3373d6df20a14ab2d3e1f
heyaviii <heyaviii01@gmail.com>
2026-06-14 22:48:53 +0530

    day8

---
+++ Array/Swap_Two_Numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 20;
+
+    cout << "Before Swap: a = " << a << ", b = " << b << endl;
+
+    int temp = a;
+    a = b;
+    b = temp;
+
+    cout << "After Swap: a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
